563fc3f5384700e8129b8fd1abf02baf81431f30
harshitamahori <harshitamahori02@gmail.com>
2025-03-01 00:16:43 +0530

    Add project files.

---
+++ DemoPlugin/CreateTask.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace for d365 interaction
+using Microsoft.Crm.Sdk;
+using Microsoft.Xrm.Sdk;
+
+namespace DemoPlugin
+{
+    public class CreateTask:IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+            //Obtain Eecution context from service provider
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if(context.InputParameters.Contains("Target")&& context.InputParameters["Target"] is Entity)
+            {
+                //obtain target entity from input parameters.
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                //If not, plugin wasn't register  correctly
+                if (entity.LogicalName != "account")
+                    return;
+                try
+                {
+                    //create a task entity to foloowup with the account customer in 7 days .
+                    Entity followup = new Entity("task");
+                    followup["subject"] = "Send e-mail to new customer";
+                    followup["description"] = "Follow with the customer";
+                    followup["scheduledstart"] = DateTime.Now.AddDays(7);
+                    followup["scheduledend"] = DateTime.Now.AddDays(7);
+                    followup["category"] = context.PrimaryEntityName;
+
+                    //Refer to the acoount in task entity.
+                    if (context.OutputParameters.Contains("id"))
+                    {
+                        //lookup
+                        Guid regardingobjectid = new Guid(context.OutputParameters["id"].ToString());
+                        string regardingobjectidType = "account";
+
+                        followup["regardingobjectid"] = new EntityReference(regardingobjectidType, regardingobjectid);
+
+                    }
+                        //Obtain the organisation service reference
+                        IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                        IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                        service.Create(followup);
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                        throw new InvalidPluginExecutionException("An error occured in followup plugin ",ex);
+                    }
+            }
+                
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ DemoPlugin/Plugins/SampleCreateRead.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk; // to implemebt IPlugin Interface
+using Microsoft.Xrm.Sdk.Query; // used for retireve operation 
+
+namespace DemoPlugin.Plugins
+{
+    public class SampleCreateRead:IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider) // prvides a mechanism to retrieve a service object
+        {
+            //defines contexual info passed to plugin at runtime .Entity , field , schema name etc
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            //This represent the factory for creating IOrganisation service instance
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            //IOrganisationService provides the programmatic access to meta data and data for oraganisation
+            IOrganizationService service = factory.CreateOrganizationService(context.InitiatingUserId);
+
+            if (context.PrimaryEntityName == "account") // provides data on execution pipleine (schema name )
+            {
+                Entity accountRecord = service.Retrieve("account", context.PrimaryEntityId, new ColumnSet("name", "telephone1"));
+                string accountName = accountRecord.GetAttributeValue<string>("name");
+                string phoneNumber = accountRecord.GetAttributeValue<string>("telephone1");
+
+                Entity contactRecord = new Entity("contact"); // local record of type contat entity
+                contactRecord["fullname"] = accountName; //assign values to this contact record
+                contactRecord["telephone1"] = phoneNumber; //assign value to phone filed
+
+                //Associate account record to contact record
+                contactRecord["parentcustomerid"] = new EntityReference("account", context.PrimaryEntityId);
+                contactRecord["accountrolecode"] = new OptionSetValue(2);
+                contactRecord["creditlimit"] = new Money(100);
+                contactRecord["lastonholdtime"] = new DateTime(2025, 01, 01);
+                contactRecord["donotphone"] = true;
+                contactRecord["numberofchildren"]= 0;
+
+                Guid contactId = service.Create(contactRecord);
+
+
+
+
+            }
+
+
+        }
+    }
+}
