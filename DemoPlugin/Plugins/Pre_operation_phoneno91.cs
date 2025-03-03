9d6af0ab95047ac1008afef7610d298e2129333f
harshitamahori <harshitamahori02@gmail.com>
2025-03-03 22:58:37 +0530

    Tried Pre-Operation : if country is India then phone no. starts with +91.

---
+++ DemoPlugin/Plugins/Pre_operation_phoneno91.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace DemoPlugin.Plugins
+{
+    public class Pre_operation_phoneno91: IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                try
+                {
+                    if(entity.LogicalName=="account" && entity.Attributes.Contains("telephone1") && entity.Attributes.Contains("address1_country"))
+                    {
+                        string phoneNumber = entity.Attributes["telephone1"].ToString();
+                        string country = entity.Attributes["address1_country"].ToString().ToLower();
+
+                        if(country=="india" && !phoneNumber.StartsWith("+91"))
+                        {
+                            entity["telephone1"] = "+91" + phoneNumber;
+
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    throw new InvalidPluginExecutionException("Error in Pre-operation Plugin: " + ex.Message);
+                }
+            }
+        }
+    }
+}
