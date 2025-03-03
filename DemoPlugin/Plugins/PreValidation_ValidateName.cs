133da4637d89197bae2d2fa2ecf6d0d06341984f
harshitamahori <harshitamahori02@gmail.com>
2025-03-03 23:25:18 +0530

    Tried Pre-Validation : Checks the name doesn't contain any number

---
+++ DemoPlugin/Plugins/PreValidation_ValidateName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace DemoPlugin.Plugins
+{
+    public class PreValidation_ValidateName:IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if(context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                if(entity.LogicalName=="account" && entity.Attributes.Contains("name"))
+                {
+                    string name = entity["name"].ToString();
+
+                    if(Regex.IsMatch(name, @"\d"))
+                    {
+                        throw new InvalidPluginExecutionException("Account name cannot contain number");
+                    }
+                }
+            }
+        }
+    }
+}
