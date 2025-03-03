cb8e8eb2dbd6852aa52dfd15aaad8bc45fd770d6
harshitamahori <harshitamahori02@gmail.com>
2025-03-03 20:47:34 +0530

    Tried Pre-validation for account table : fax field

---
+++ DemoPlugin/Plugins/Pre_Validation_Fax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CodeDom;
+using Microsoft.Xrm.Sdk;
+
+namespace DemoPlugin.Plugins
+{
+    public class Pre_Validation_Fax:IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if(context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                try
+                {
+                    Entity entity = (Entity)context.InputParameters["Target"];
+
+                    if (entity.LogicalName != "account") return;
+
+                    if (!entity.Attributes.Contains("fax"))
+                    {
+                        throw new InvalidPluginExecutionException("Fax number is required before creating account");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    throw new InvalidPluginExecutionException($"Pre-validation Error: {ex.Message}");
+
+                }
+            }
+            
+        }
+    }
+}
