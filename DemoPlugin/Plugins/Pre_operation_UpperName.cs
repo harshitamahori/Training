b638c8b4644eea72218a37ac04a0612e932ee118
harshitamahori <harshitamahori02@gmail.com>
2025-03-03 22:15:51 +0530

    Tried Pre-operation : change accont namr to uppercase while creating a record

---
+++ DemoPlugin/Plugins/Pre_operation_UpperName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace DemoPlugin.Plugins
+{
+    public class Pre_operation_UpperName : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                try
+                {
+                    if (entity.LogicalName == "account" && entity.Attributes.Contains("name"))
+                    {
+                        string accountName = entity.Attributes["name"].ToString();
+                        entity.Attributes["name"] = accountName.ToUpper();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidPluginExecutionException("Error in Pre-Operation: " + ex.Message);
+                }
+            }
+        } 
+    }
+}
