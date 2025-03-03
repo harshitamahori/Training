c035f3c6b3d4e97e4252743696a44979c12acdb1
harshitamahori <harshitamahori02@gmail.com>
2025-03-03 21:37:04 +0530

    Prev-Validation for name field.

---
+++ DemoPlugin/Plugins/Pre_Validation_Name.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace DemoPlugin.Plugins
+{
+    public class Pre_Validation_Name:IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if(context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                if (entity.LogicalName != "account") return;
+
+                if (!entity.Attributes.Contains("name") || string.IsNullOrWhiteSpace(entity["name"].ToString()))
+                {
+                    throw new InvalidPluginExecutionException("Name is required");
+                }
+
+            }
+        }
+    }
+}
