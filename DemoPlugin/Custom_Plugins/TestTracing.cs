0c7a1944d1e6777357ceb314ba3fe67577fe59d3
harshitamahori <harshitamahori02@gmail.com>
2025-03-06 02:23:52 +0530

    Demo for ITracingServices to see the logs in Dynamics365.

---
+++ DemoPlugin/Custom_Plugins/TestTracing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+namespace DemoPlugin.Custom_Plugins
+{
+    public class TestTracing:IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            tracingService.Trace("Plugin Execution Started");
+
+            
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            
+            if (context.PrimaryEntityName == "cr371_customer_p" && context.MessageName.ToLower() == "delete")
+            {
+                tracingService.Trace("Deleting Customer record...");
+
+                //throw new InvalidPluginExecutionException("Test Exception: Customer deletion is being tracked.");
+            }
+            tracingService.Trace("Task Completed");
+        }
+    }
+}
