163d5e5036d981c62ab886d6ef1b495677e5d310
harshitamahori <harshitamahori02@gmail.com>
2025-03-11 01:37:55 +0530

    Tried to apply the concept of Secure and Unsecure Configuration using Config and Helper classes.

---
+++ DemoPlugin/Helper/JSONHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace DemoPlugin.Helper
+{
+    //Every JSON it pass, it will deserialize this obj and give the value of particular object pass to any function
+    public static class JSONHelper // static class loaded immediately so then normal class so 
+    {
+        public static T Deserialize<T>(string json) // static function (generic func for any kind of object)
+        {
+            using (var memoryStream = new MemoryStream(Encoding.Unicode.GetBytes(json))) // dispose off  memory stream 
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T)); // converts json into object
+                return (T) serializer.ReadObject(memoryStream);
+            }
+        }
+
+    }
+}
---
+++ DemoPlugin/Secure_Unsecure_Config/PluginConfigurationSample.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+using DemoPlugin.DataContract;
+using DemoPlugin.Helper;
+
+namespace DemoPlugin.Secure_Unsecure_Config
+{
+    public class PluginConfigurationSample:IPlugin
+    {
+        //class variables 
+        public readonly string unsecureValue;
+        public readonly string secureValue;
+        public PluginConfigurationSample(string unsecureConfig, string secureConfig)//{"Name":"XYZ"}
+        {
+            if (String.IsNullOrWhiteSpace(unsecureConfig)) //if null or now value
+            {
+                unsecureValue = string.Empty;
+
+            }
+            else
+            {
+               Config data = JSONHelper.Deserialize<Config>(unsecureConfig);
+                unsecureValue = data.Name;
+
+            }
+            if (string.IsNullOrWhiteSpace(secureConfig))
+            {
+                secureValue = string.Empty;
+            }
+            else
+            {
+                Config data = JSONHelper.Deserialize<Config>(secureConfig);
+                secureValue = data.Name;
+            }
+
+        }
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+                if (context.Depth > 1) return;
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is EntityReference)
+                {
+                    IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = factory.CreateOrganizationService(context.InitiatingUserId);
+
+                    EntityReference applicationEntityRef = context.InputParameters["Target"] as EntityReference;
+
+                    Entity updateApplication = new Entity(applicationEntityRef.LogicalName);
+                    updateApplication.Id = applicationEntityRef.Id;
+                    updateApplication["cr371_customername"] = secureValue + " " + unsecureValue;
+                    service.Update(updateApplication);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+
+            }
+
+        }
+    }
+}
