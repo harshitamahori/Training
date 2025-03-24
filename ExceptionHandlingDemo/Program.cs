01f76e3d344d183c84ff025430dee0717e1fd0b4
harshitamahori <harshitamahori02@gmail.com>
2025-03-25 03:58:48 +0530

    Removed Unnecessary variable

--- ExceptionHandlingDemo/Program.cs
+++ ExceptionHandlingDemo/Program.cs
@@ -22,7 +22,7 @@ class Program
 
             ValidateNumber(num);  // This may throw an exception
         }
-        catch (FormatException ex)
+        catch (FormatException)
         {
             Console.WriteLine("Error: Please enter a valid integer.");
         }
